22efd848e9b37dfeb2b175ba4f1bdda8d3891eb9
rohit-kashyap-1 <r23.tgc@gmail.com>
2025-01-03 19:24:20 +0530

    updated

--- doctors.php
+++ doctors.php
@@ -59,6 +59,9 @@ include 'header.php';
                 <?php include('doctor-list-item.php'); ?>
                 <?php include('doctor-list-item.php'); ?>
                 <?php include('doctor-list-item.php'); ?>
+                <div class="text-center">
+                    <a href="" class="btn btn-lg btn-primary" style="font-size: 15px;">Show more Doctors...</a>
+                </div>
             </div>
             <div class="hospital-sidebar">
                 <div class="bg-primary p-4 rounded">
@@ -73,6 +76,79 @@ include 'header.php';
         </div>
     </div>
 </section>
+<section class="section pt-0">
+    <div class="container">
+      <div class="hospital-x bg-light border rounded p-4 mb-4">
+        <h4 class=" mb-3 heading">Top Doctors in India</h4>
+        <ul>
+          <li><a href="https://www.MedTravely.com/hospitals/medanta-medicity-gurgaon" title="Medanta - The Medicity">Best Cardiac Surgeons in India</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/indraprastha-apollo-hospital-new-delhi"
+              title="Indraprastha Apollo Hospital">Best Pediatric Cardiac Surgeons in India</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/fortis-escorts-heart-institute-new-delhi"
+              title="Fortis Escorts Heart Institute">Best Kidney Specialists In India</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/artemis-hospital-gurgaon" title="Artemis Hospital">Best Urologist in India</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/max-super-speciality-hospital-saket-new-delhi"
+              title="Max Super Speciality Hospital">Max Super Speciality Hospital</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/fortis-memorial-research-institute-gurgaon"
+              title="Fortis Memorial Research Institute">Fortis Memorial Research Institute</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/manipal-hospital-bangalore"
+              title="Manipal Hospital (Old Airport Road)">Manipal Hospital (Old Airport Road)</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/apollo-hospitals-greams-road-chennai"
+              title="Apollo Hospitals, Greams Road">Apollo Hospitals, Greams Road</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/kokilaben-dhirubhai-ambani-hospital-mumbai"
+              title="Kokilaben Dhirubhai Ambani Hospital">Kokilaben Dhirubhai Ambani Hospital</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/apollo-hospitals-hyderabad"
+              title="Apollo Hospitals, Jubilee Hills Hyderabad">Apollo Hospitals, Jubilee Hills Hyderabad</a></li>
+        </ul>
+      </div>
+      <div class="hospital-x bg-light border rounded p-4 mb-4">
+        <h4 class=" mb-3 heading">Top Doctors in India</h4>
+        <ul>
+          <li><a href="https://www.MedTravely.com/hospitals/medanta-medicity-gurgaon" title="Medanta - The Medicity">Best Cardiac Surgeons in India</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/indraprastha-apollo-hospital-new-delhi"
+              title="Indraprastha Apollo Hospital">Best Pediatric Cardiac Surgeons in India</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/fortis-escorts-heart-institute-new-delhi"
+              title="Fortis Escorts Heart Institute">Best Kidney Specialists In India</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/artemis-hospital-gurgaon" title="Artemis Hospital">Best Urologist in India</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/max-super-speciality-hospital-saket-new-delhi"
+              title="Max Super Speciality Hospital">Max Super Speciality Hospital</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/fortis-memorial-research-institute-gurgaon"
+              title="Fortis Memorial Research Institute">Fortis Memorial Research Institute</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/manipal-hospital-bangalore"
+              title="Manipal Hospital (Old Airport Road)">Manipal Hospital (Old Airport Road)</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/apollo-hospitals-greams-road-chennai"
+              title="Apollo Hospitals, Greams Road">Apollo Hospitals, Greams Road</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/kokilaben-dhirubhai-ambani-hospital-mumbai"
+              title="Kokilaben Dhirubhai Ambani Hospital">Kokilaben Dhirubhai Ambani Hospital</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/apollo-hospitals-hyderabad"
+              title="Apollo Hospitals, Jubilee Hills Hyderabad">Apollo Hospitals, Jubilee Hills Hyderabad</a></li>
+        </ul>
+      </div>
+      <div class="hospital-x bg-light border rounded p-4 mb-4">
+        <h4 class=" mb-3 heading">Top Doctors in India</h4>
+        <ul>
+          <li><a href="https://www.MedTravely.com/hospitals/medanta-medicity-gurgaon" title="Medanta - The Medicity">Best Cardiac Surgeons in India</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/indraprastha-apollo-hospital-new-delhi"
+              title="Indraprastha Apollo Hospital">Best Pediatric Cardiac Surgeons in India</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/fortis-escorts-heart-institute-new-delhi"
+              title="Fortis Escorts Heart Institute">Best Kidney Specialists In India</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/artemis-hospital-gurgaon" title="Artemis Hospital">Best Urologist in India</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/max-super-speciality-hospital-saket-new-delhi"
+              title="Max Super Speciality Hospital">Max Super Speciality Hospital</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/fortis-memorial-research-institute-gurgaon"
+              title="Fortis Memorial Research Institute">Fortis Memorial Research Institute</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/manipal-hospital-bangalore"
+              title="Manipal Hospital (Old Airport Road)">Manipal Hospital (Old Airport Road)</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/apollo-hospitals-greams-road-chennai"
+              title="Apollo Hospitals, Greams Road">Apollo Hospitals, Greams Road</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/kokilaben-dhirubhai-ambani-hospital-mumbai"
+              title="Kokilaben Dhirubhai Ambani Hospital">Kokilaben Dhirubhai Ambani Hospital</a></li>
+          <li><a href="https://www.MedTravely.com/hospitals/apollo-hospitals-hyderabad"
+              title="Apollo Hospitals, Jubilee Hills Hyderabad">Apollo Hospitals, Jubilee Hills Hyderabad</a></li>
+        </ul>
+      </div>
+    </div>
+  </section>
 <section class="section bg-light">
     <div class="container">
         <h4 class="mb-4 heading">Frequently Asked Questions (FAQ’s)</h4>
